b8c109a6e38db03b5cf2a74b92a245c0afd580d6
ftroka <ftroka25@epoka.edu.al>
2026-06-19 23:32:20 +0200

    Added assignments API

---
+++ backend/UniTrack.Api/Controllers/AssignmentsController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniTrack.Api.Data;
+using UniTrack.Api.Models;
+
+namespace UniTrack.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AssignmentsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public AssignmentsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<CourseAssignment>>> GetAssignments()
+    {
+        var assignments = await _context.Assignments.ToListAsync();
+
+        return Ok(assignments);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<CourseAssignment>> GetAssignmentById(int id)
+    {
+        var assignment = await _context.Assignments.FindAsync(id);
+
+        if (assignment == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(assignment);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CourseAssignment>> CreateAssignment(CourseAssignment assignment)
+    {
+        _context.Assignments.Add(assignment);
+
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetAssignmentById), new { id = assignment.Id }, assignment);
+    }
+}
