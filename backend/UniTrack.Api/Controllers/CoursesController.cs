26f8b1ca7f2a6eff257ab30f3380175e6408f9c3
ftroka <ftroka25@epoka.edu.al>
2026-06-19 23:26:35 +0200

    Added courses API

---
+++ backend/UniTrack.Api/Controllers/CoursesController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniTrack.Api.Data;
+using UniTrack.Api.Models;
+
+namespace UniTrack.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CoursesController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public CoursesController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Course>>> GetCourses()
+    {
+        var courses = await _context.Courses.ToListAsync();
+
+        return Ok(courses);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Course>> GetCourseById(int id)
+    {
+        var course = await _context.Courses.FindAsync(id);
+
+        if (course == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(course);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Course>> CreateCourse(Course course)
+    {
+        _context.Courses.Add(course);
+
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetCourseById), new { id = course.Id }, course);
+    }
+}
